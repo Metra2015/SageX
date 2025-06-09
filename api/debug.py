6c208c7bcf337480cec6ec9f1e3a587acc6f42f5
Metra2015 <artem.yushak@gmail.com>
2025-06-09 14:06:59 -0400

    Update debug.py

--- api/debug.py
+++ api/debug.py
@@ -3,13 +3,12 @@ from sqlalchemy import text
 from core.database import SessionLocal
 from core.config import settings
 
-
+router = APIRouter()
 
 
 @router.get("/db-url", summary="Show current DB connection URL")
 def get_db_url():
     return {"db_url": settings.DB_URL}
-router = APIRouter()
 
 @router.get("/db-check", summary="Check database connectivity")
 def check_db():
